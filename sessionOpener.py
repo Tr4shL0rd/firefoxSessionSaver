2cccdabb819cf18a5dd9bcb4307ae8b2fcb656b2
Tr4shL0rd <ketil04@gmail.com>
2021-09-17 15:27:07 +0200

    Add files via upload

--- sessionOpener.py
+++ sessionOpener.py
@@ -1,57 +1,25 @@
-import os
+import webbrowser as wb
 from platform import uname
 
 debug = False
 if debug == True: print("Debug Mode True")
 
-#Detects OS
-def in_wsl() -> bool:
-    return "Microsoft" in uname().release
-def in_nt() -> bool:
-    return "nt" in os.name
-def in_nix() -> bool:
-    return "posix" in os.name
-if debug == True: print("wsl? {}\nnt? {}\nnix? {}".format(in_wsl(),in_nt(),in_nix()))
-
-
-#Sets Firefox Path Depending on the OS
-if in_wsl() == True & in_nix() == True:
-    FFPATH = "/mnt/c/'Program Files'/'Mozilla Firefox'/./firefox.exe"
-if in_nt() == True:
-    FFPATH = "C:/'Program Files'/'Mozilla Firefox'/firefox.exe"
-if in_nix() == True & in_wsl() == False:
-    FFPATH = "/usr/bin/firefox"
-
-if debug == True: print("FF PATH:",FFPATH)
-#Cleaning up path for checking
-PathCheck = FFPATH.replace("'", "")
-if os.path.isfile(PathCheck) == False:
-    print("Cant Find firefox.exe!")
-    exit()
-if debug == True:
-    if "mnt" in FFPATH:
-        print("USING WSL")
-    elif "C:" in FFPATH:
-        print("USING NT")
-    else:
-        print("USING NIX")
-#lists the urls and askes if they should be opened
 try:
     f = open("urlListFile.txt", "r")
     url = f.readlines()
 
-    print("######### {} URLS #########".format(len(url)))
+    print(f"######### {len(url)} URLS #########")
     for i in range(len(url)):
         print(url[i])
     print("###########################")
-    yn = input("open URLS?(y/n):  ".format(len(url))).lower()
+    yn = input("open URLS?(Y/n):  ").lower()
 
-    if yn == "y":
+    if yn == "y" or yn == "":
         if debug == False:
             for urlList in range(len(url)):
-                os.system("{} {}".format(FFPATH,url[urlList]))
+                wb.get("firefox").open(url[urlList])
         else:
             for i in range(len(url)):
-                print("opened {}".format(url[i]))
+                print(f"opened {url[i]}")
 except KeyboardInterrupt:
     print("\n#### EXITING ####")
