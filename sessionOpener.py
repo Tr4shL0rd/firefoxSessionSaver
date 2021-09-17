dafcd08706676f5f5f2e8e6f9a577e99a1a61e8b
Tr4shL0rd <ketil04@gmail.com>
2021-09-17 15:44:40 +0200

    Add files via upload

--- sessionOpener.py
+++ sessionOpener.py
@@ -1,25 +1,14 @@
 import webbrowser as wb
-from platform import uname
 
-debug = False
-if debug == True: print("Debug Mode True")
+f = open("urlListFile.txt", "r")
+url = f.readlines()
 
-try:
-    f = open("urlListFile.txt", "r")
-    url = f.readlines()
+print(f"######### {len(url)} URLS #########")
+for URL in url:
+    print(URL)
+print("###########################")
+yn = input("open URLS?(Y/n): ").lower()
 
-    print(f"######### {len(url)} URLS #########")
-    for i in range(len(url)):
-        print(url[i])
-    print("###########################")
-    yn = input("open URLS?(Y/n):  ").lower()
-
-    if yn == "y" or yn == "":
-        if debug == False:
-            for urlList in range(len(url)):
-                wb.get("firefox").open(url[urlList])
-        else:
-            for i in range(len(url)):
-                print(f"opened {url[i]}")
-except KeyboardInterrupt:
-    print("\n#### EXITING ####")
+if yn == "y" or yn == "":
+    for urlList in url:
+        wb.get("firefox").open(urlList)
